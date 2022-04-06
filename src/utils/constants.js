c6857f0695a3457f58603cfade7f126ab2cc2510
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-06 23:42:04 +0300

    added new constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,34 @@ export const STATUS = {
   ACTIVE: 'active',
   ARCHIVED: 'archived',
 };
+
+export const HEAD_BUTTONS_TYPE = {
+  CONTROL: 'control',
+  AGREEMENT: 'agreement',
+};
+
+export const TABLE_TYPE = {
+  ACTIVE: 'active-notes',
+  ARCHIVE: 'archive',
+};
+
+export const TABLE_HEAD_CAPTIONS = [
+  'icon',
+  'name',
+  'created',
+  'category',
+  'content',
+  'dates',
+];
+
+export const OPERATION_TYPE = {
+  ARCHIVE: 'archive',
+  DELETE: 'remove',
+};
+
+export const CATEGORIES = {
+  TASK: 'Task',
+  RANDOM_THOUGHT: 'Random Thought',
+  IDEA: 'Idea',
+  QUOTA: 'Quote',
+};
