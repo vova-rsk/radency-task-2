b0a83af922655f55649eba77a7c63547b02394be
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-07 23:37:30 +0300

    removed unused

--- src/pages/ArchivePage.jsx
+++ src/pages/ArchivePage.jsx
@@ -1,14 +1,10 @@
-import { useSelector } from 'react-redux';
 import Table from '../components/UniversalTable/Table';
-import { getFilteredNotes } from '../redux/notes/notes-selectors';
 import { TABLE_TYPE } from '../utils/constants';
 
 export default function ArchivePage() {
-  const notesList = useSelector(getFilteredNotes);
-
   return (
     <div>
-      <Table tableData={notesList} tableType={TABLE_TYPE.NOTES} />
+      <Table tableType={TABLE_TYPE.NOTES} />
     </div>
   );
 }
