d6cfe34712e49cc997dc9aa11343dfe585e87d85
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-09 19:59:25 +0300

    fix regex

--- src/utils/getDateIntervalFromContent.js
+++ src/utils/getDateIntervalFromContent.js
@@ -1,7 +1,13 @@
+// const REGEX = /(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}/g; !!!!!!!!!
+
+// const REGEX = /(3[01]|2[0-9]|1[0-9]|0[1-9]|[1-9])\/(1[0-2]|0[1-9]|[1-9])\/[1-9][0-9]{3}/g;
+
 const REGEX =
-  /((?=\d{4})\d{4}|(?=[a-zA-Z]{3})[a-zA-Z]{3}|\d{2})((?=\/)\/|\-)((?=[0-9]{2})[0-9]{2}|(?=[0-9]{1,2})[0-9]{1,2}|[a-zA-Z]{3})((?=\/)\/|\-)((?=[0-9]{4})[0-9]{4}|(?=[0-9]{2})[0-9]{2}|[a-zA-Z]{3})/g;
+  /([1-9]|0[1-9]|1[0-9]|2[0-9]|3[01])\/([1-9]|0[1-9]|1[0-2])\/[1-9][0-9]{3}/g;
 
 export default function getDateIntervalFromContent(text) {
   const result = text.match(REGEX);
+  console.log(result);
+  console.log(REGEX.test(text));
   return result ? [result[0], result[1]] : null;
 }
