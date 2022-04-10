a46b477af9edb0ce55a34098b3002dbff7547729
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-11 00:06:12 +0300

    converted to ts

--- src/components/Table/TableBody/TableBody.styled.tsx
+++ src/components/Table/TableBody/TableBody.styled.tsx
@@ -2,13 +2,19 @@ import styled from '@emotion/styled';
 import { TableCell } from '@mui/material';
 import { TABLE_TYPE } from '../../../utils/constants';
 
-const StyledTableCell = styled(TableCell)`
+interface ComponentProps {
+  index?: number;
+  type?: string;
+}
+
+const StyledTableCell = styled(TableCell)<ComponentProps>`
   margin: 0;
   padding: 5px 10px;
   font-weight: 500;
   font-size: 14px;
   line-height: 1.167;
-  color: ${props => (props.idx === 1 ? '#000' : 'rgb(131, 131, 131)')};
+  
+  color: ${props => (props.index === 1 ? '#000' : 'rgb(131, 131, 131)')};
 
   &:first-of-type {
     width: 40px;
--- src/components/Table/TableBody/TableBody.tsx
+++ src/components/Table/TableBody/TableBody.tsx
@@ -3,7 +3,7 @@ import { TableBody as Body } from '@mui/material';
 import TableRow from '@mui/material/TableRow';
 import StyledTableCell from './TableBody.styled';
 import CategoryIcon from '../CategoryIcon';
-import BodyControlButtons from '../BodyControlButtons/index.ts';
+import BodyControlButtons from '../BodyControlButtons';
 import { TABLE_TYPE } from '../../../utils/constants';
 import {
   getFilteredNotes,
@@ -11,7 +11,31 @@ import {
 } from '../../../redux/notes/notes-selectors';
 import updateNotesStructure from '../../../utils/updateNotesStructure';
 
-export default function TableBody(props) {
+interface IProps { 
+  tableType: string;
+  isControlButtonsShow: boolean;
+  handleSwitchNoteSelection:(id:string) => void;
+}
+
+interface ISummary { 
+  id: string;
+  category: string;
+  active: number;
+  archived: number;
+}
+
+interface INote { 
+  id: string;
+  name: string;
+  created: string;
+  category: string;
+  content: string;
+  dates: string;
+}
+
+type RowData = ISummary | INote;
+
+const TableBody = (props:IProps) => {
   const notesList = useSelector(getFilteredNotes);
   const summaryList = useSelector(getSummary);
 
@@ -21,17 +45,15 @@ export default function TableBody(props) {
     ? updateNotesStructure(notesList)
     : summaryList;
 
-  const createRowMarkup = note => {
-    const keys = Object.keys(note);
-
-    return keys.map((key, idx) =>
+  const createRowMarkup = (rowData:RowData) => {
+    return Object.values(rowData).map((value:string|number, idx) =>
       idx === 0 ? (
-        <StyledTableCell key={idx} idx={idx} type={tableType}>
-          <CategoryIcon categoryName={note.category} />
+        <StyledTableCell key={idx} index={idx} type={tableType}>
+            <CategoryIcon categoryName={rowData.category} />
         </StyledTableCell>
       ) : (
-        <StyledTableCell key={idx} idx={idx} type={tableType}>
-          {note[key]}
+        <StyledTableCell key={idx} index={idx} type={tableType}>
+              {value}
         </StyledTableCell>
       ),
     );
@@ -39,19 +61,19 @@ export default function TableBody(props) {
 
   const bodyMarkup =
     tableData &&
-    tableData.map(note => {
-      const rowMarkup = createRowMarkup(note);
+    tableData.map((rowData) => {
+      const rowMarkup = createRowMarkup(rowData);
 
       return (
         <TableRow
-          key={note.id}
+          key={rowData.id}
           sx={{ backgroundColor: 'rgba(217, 204, 255, 0.3)' }}
         >
           {rowMarkup}
           {isNotesTable && (
             <StyledTableCell key="CtrlBtns">
               <BodyControlButtons
-                noteId={note.id}
+                noteId={rowData.id}
                 isCtrlButtonsShow={isControlButtonsShow}
                 handleSwitchNoteSelection={handleSwitchNoteSelection}
               />
@@ -62,4 +84,6 @@ export default function TableBody(props) {
     });
 
   return <Body>{bodyMarkup}</Body>;
-}
+};
+
+export default TableBody;
