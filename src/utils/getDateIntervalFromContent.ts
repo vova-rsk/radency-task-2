20e059b2a0a02cb8475e0dcae1acb87277e1b46f
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-10 11:21:18 +0300

    fix

--- src/utils/getDateIntervalFromContent.ts
+++ src/utils/getDateIntervalFromContent.ts
@@ -1,12 +1,10 @@
-// const REGEX = /(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d{2}/g; !!!!!!!!!
-
-// const REGEX = /(3[01]|2[0-9]|1[0-9]|0[1-9]|[1-9])\/(1[0-2]|0[1-9]|[1-9])\/[1-9][0-9]{3}/g;
-
 const REGEX =
-  /([1-9]|0[1-9]|1[0-9]|2[0-9]|3[01])\/([1-9]|0[1-9]|1[0-2])\/[1-9][0-9]{3}/g;
+  /([1-9]|0[1-9]|1[0-9]|2[0-9]|3[0-1]?)\/([1-9]|0[1-9]|1[0-2]?)\/[1-9][0-9]{3}/g;
 
-export default function getDateIntervalFromContent(text:string) {
+const getDateIntervalFromContent = (text:string) => {
   const result = text.match(REGEX);
 
   return result ? [result[0], result[1]] : null;
-}
+};
+
+export default getDateIntervalFromContent;
