767b6b164a5fa3d2b61b58418626bf877e40c8e9
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-10 19:43:05 +0300

    fix

--- src/redux/notes/notes-actions.ts
+++ src/redux/notes/notes-actions.ts
@@ -10,16 +10,21 @@ interface INote {
   status: string;
 }
 
-interface IReplaceObj{ 
+interface IUpdateNote { 
+  id: string;
+  status: string;
+}
+
+interface IReplaceNote{ 
   ids: string[];
   status: string;
 }
 
 export const addNote = createAction<INote, 'note/add'>('note/add');
 export const removeNote = createAction<string,'note/remove'>('note/remove');
-export const updateNote = createAction<INote,'note/update'>('note/update');
+export const updateNote = createAction<IUpdateNote,'note/update'>('note/update');
 export const changeFilter = createAction<string,'filter/change'>('filter/change');
 export const removeNotesList = createAction<string[],'notes-list/remove'>('notes-list/remove');
-export const replaceNotesList = createAction<IReplaceObj,'notes-list/replace'>('notes-list/replace');
+export const replaceNotesList = createAction<IReplaceNote,'notes-list/replace'>('notes-list/replace');
 export const changeButtonsLockStatus = createAction<undefined,'buttons-lock/change'>('buttons-lock/change');
 export const changeCreationBarVisibility = createAction<undefined,'modal/visibility change'>('modal/visibility change');
