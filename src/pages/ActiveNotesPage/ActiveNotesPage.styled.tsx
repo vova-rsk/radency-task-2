49fb3b094d785a9a9d529809f9018c2da36c287e
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-11 22:53:09 +0300

    update styles

--- src/pages/ActiveNotesPage/ActiveNotesPage.styled.tsx
+++ src/pages/ActiveNotesPage/ActiveNotesPage.styled.tsx
@@ -2,12 +2,12 @@ import styled from '@emotion/styled';
 
 const Container = styled.div`
   padding: 40px 0;
-  min-height:74px;
-
+  
   & > div:nth-of-type(2) {
     display: flex;
     justify-content: end;
     margin-top: 20px;
+    min-height:74px;
   }
 
   & > div:last-of-type {
--- src/pages/ArchivePage/ArchivePage.styled.tsx
+++ src/pages/ArchivePage/ArchivePage.styled.tsx
@@ -2,11 +2,12 @@ import styled from '@emotion/styled';
 
 const Container = styled.div`
   padding: 40px 0;
-
-    & > div:nth-of-type(2) {
+  
+  & > div:nth-of-type(2) {
     display: flex;
     justify-content: end;
     margin-top: 20px;
+    min-height:74px;
   }
 `;
 
