b92b2df79d73671905cc01cdc71476b476d06e2b
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-20 17:12:05 +0300

    added category icon story

---
+++ src/components/Table/CategoryIcon/CategoryIcon.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import CategoryIcon from './CategoryIcon';
+import { CATEGORIES } from '../../../utils/constants';
+
+const { TASK, RANDOM_THOUGHT, IDEA, QUOTA } = CATEGORIES;
+
+const exportedComponent = {
+    title: 'TABLE/CategoryIcon',
+    component: CategoryIcon,
+    argTypes: {
+        categoryName:{ 
+            type: 'string',
+            description: 'Sets the custom icon or default to display',
+            defaultValue: '',
+            options:[TASK, RANDOM_THOUGHT, IDEA, QUOTA, 'default'],
+            control: {
+                type:'radio'
+            }
+        }
+    }
+} as ComponentMeta<typeof CategoryIcon>;
+
+export default exportedComponent;
+
+const Template: ComponentStory<typeof CategoryIcon> = (args) => <CategoryIcon {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+   categoryName:''
+};
